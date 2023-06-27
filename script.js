c6105988febbb99db8e2ba99e69b292075d2bca3
Vlad Balas <balash_vlad@yahoo.com>
2023-06-27 17:16:26 +0100

    JS - Canvas Setup

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const downloadBtn = document.getElementById('download');
 const { body } = document;
 
 // Global Variables
-
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+const context = canvas.getContext('2d');
 let currentSize = 10;
 let bucketColor = '#FFFFFF';
 let currentColor = '#A51DAB';
@@ -63,10 +65,10 @@ let currentColor = '#A51DAB';
 
 // Create Canvas
 function createCanvas() {
-  // canvas.width = ;
-  // canvas.height = ;
-  // context.fillStyle = ;
-  // context.fillRect();
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 50;
+  context.fillStyle = bucketColor;
+  context.fillRect(0, 0, canvas.width, canvas.height); // deciding what the canvas dimensions should be based on the window dimensions => canvas fully responsive
   body.appendChild(canvas);
 
 }
